2806232892811b84b5054812cb9a73a91ffc5b95
Andrew Piper <pipera@uw.edu>
2018-05-14 13:06:29 -0700

    removed unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    <!--albums-block
-      v-if="!album_selected"
-      v-bind:album_list="albums"
-      :album_source="api">
-    </albums-block>
-    <images-block 
-      v-else-if="album_selected"
-      v-bind:title="album_title"
-      v-bind:images_source="source" 
-      v-bind:images_source_type="type" 
-      v-bind:grid_width="grid_width"
-      v-bind:image_count="count">
-    </images-block-->
     <router-view></router-view>
     <modal-block>
     </modal-block>
@@ -31,45 +18,17 @@ export default {
     return {
       // number of images to show in a row
       grid_width: 3,
-      type: 'url',
-      //album_uri: config.data.source,
-      album_selected: false,
-      album_title: "",
       albums: "",
       source: "",
       count: 0,
     }
   },
   computed: {
-    source2 () {
-      if (imageData) {
-        return imageData
-      }
-      return config.data.source
-    },
-    album_list () {
-    },
-    asource () {
-      if (this.album_uri) {
-        return this.album_uri
-      }
-      return albumData
-    },
     ...mapState([
       'api',
     ]),
   },
   methods: {
-    openAlbum (album) {
-      this.type = "url"
-      this.album_title = album.name
-      this.source = `${this.album_uri}/images/api/albums/${album.clean_name}`
-      this.album_selected = true
-      this.count = album.count
-    },
-    closeAlbum () {
-      this.album_selected = false
-    }
   },
   components: {
     AlbumsBlock,
@@ -77,12 +36,8 @@ export default {
     ModalBlock
   },
   created () {
-    this.evtHub.$on("selectAlbum", this.openAlbum)
-    this.evtHub.$on("back-to-albums", this.closeAlbum)
   },
   destroyed () {
-    this.evtHub.$off("selectAlbum")
-    this.evtHub.$off("back-to-albums")
   }
 }
 </script>
